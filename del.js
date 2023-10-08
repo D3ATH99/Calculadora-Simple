273adb960a2335bc6322c03c01a72b6eac9479b7
Nareme <narememartincabrera@gmail.com>
2023-10-08 13:40:21 +0100

    delete

--- del.js
+++
@@ -1,8 +0,0 @@
-// Función para borrar el último carácter del campo de entrada
-function deleteLastCharacter() {
-    var inputField = document.calculator.ans;
-    var currentValue = inputField.value;
-    if (currentValue.length > 0) {
-        inputField.value = currentValue.slice(0, -1);
-    }
-}
